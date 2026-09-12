6e0ca853866355892d02f534877a53f89ce20e97
leonnies <masterlomaster645@gmail.com>
2026-09-12 21:12:16 +0500

    task2 added

---
+++ tyuiu.denisovld.sprint0.task2.v0.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tyuiu.denisovld.sprint0.task2.v0.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tyuiu.denisovld.sprint0.task2.v0.test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b2e574aa-49b8-4747-a610-71a620c8ea17")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ tyuiu.denisovld.sprint0.task2.v0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+//using System.Text;
+using System.Threading.Tasks;
+
+using tyuiu.denisovld.sprint0.Task2.v0.lib;
+
+
+namespace tyuiu.denisovld.sprint0.task2.v0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(DataService.GetMessage("John"));
+            Console.ReadKey();
+        }
+    }
+}
