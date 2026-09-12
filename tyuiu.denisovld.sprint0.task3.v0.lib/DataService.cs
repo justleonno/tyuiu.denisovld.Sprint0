62350a1a5793dd9bbf9a8fd7167cd5ac0cf6b2d1
leonnies <masterlomaster645@gmail.com>
2026-09-12 22:16:08 +0500

    Reapply "task3 added"
    This reverts commit 0eb3fab5b8f6c8c5f5b9b6b6660b01360996d3d7.

---
+++ tyuiu.denisovld.sprint0.task3.v0.lib/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.denisovld.sprint0.task3.v0.lib
+{
+    public class DataService
+    {
+        public static int Sum(int a, int b)
+        {  return a + b; 
+        }
+    }
+}
