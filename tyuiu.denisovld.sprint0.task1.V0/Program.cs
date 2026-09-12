6a8d0c72880c5d6b79d45e373531dd95a44bed56
leonnies <masterlomaster645@gmail.com>
2026-09-12 18:08:43 +0500

    task1 added

---
+++ tyuiu.denisovld.sprint0.task1.V0/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.denisovld.sprint0.task1.V0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello, world!");
+            string name = "John";
+            int age = 20;
+            Console.WriteLine("Здраствуйте, меня зовут" + name + "Мне" + age + "лет");
+            Console.ReadKey();
+        }
+    }
+}
