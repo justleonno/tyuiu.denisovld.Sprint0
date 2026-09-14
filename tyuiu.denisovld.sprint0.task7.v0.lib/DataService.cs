ba40797a758454118808997c108a5414ba4f3c21
leonnies <masterlomaster645@gmail.com>
2026-09-15 02:59:40 +0500

    task7 added

---
+++ tyuiu.denisovld.sprint0.task7.v0.lib/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.denisovld.sprint0.task7.v0.lib
+{
+    public class DataService
+    {
+        public static int[] AdditionArrays(int[] num1, int[] num2)
+        {
+            int[] resultArray = new int[5];
+            for (var i = 0;  i < num1.Length; i++)
+            {
+                resultArray[i] = num1[i] + num2[i];
+            }
+            return resultArray;
+        }
+    }
+}
---
+++ tyuiu.denisovld.sprint0.task7.v0.test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("tyuiu.denisovld.sprint0.task7.v0.test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("tyuiu.denisovld.sprint0.task7.v0.test")]
+[assembly: AssemblyCopyright("Copyright ©  2026")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9c63f853-593a-42e7-b841-e14ec7650c9e")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
