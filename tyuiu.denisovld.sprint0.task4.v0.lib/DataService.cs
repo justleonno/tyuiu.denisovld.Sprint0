9da4336ee2b7629f5b648893d9d1dd2890bee26f
leonnies <masterlomaster645@gmail.com>
2026-09-14 23:49:38 +0500

    task4 added

---
+++ tyuiu.denisovld.sprint0.task4.v0.lib/DataService.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tyuiu.denisovld.sprint0.task4.v0.lib
+{
+    public class DataService
+    {
+        public static int Addition(int a, int b)
+        { return a + b; }
+
+        public static int Subtraction(int a, int b)
+        { return a - b; }
+
+        public static int Multiplication(int a, int b)
+        { return a * b; }
+
+        public static double Division(int a, int b)
+        { return a / b; }
+    }
+}
